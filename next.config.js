91a3d8c3f398a8d65c0f25612de2612fece12236
maito1201 <itomasa0915@gmail.com>
2022-10-15 22:06:33 +0900

    add gh-page config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : '/'
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: '/basicauth_example',
+  assetPrefix: urlPrefix,
+  basePath: process.env.GITHUB_ACTIONS && '/basicauth_example',
+  trailingSlash: true,
+  exportPathMap: async function() {
+    return {
+      "/": { page: "/" }
+    }
+  }
 }
 
 module.exports = nextConfig
